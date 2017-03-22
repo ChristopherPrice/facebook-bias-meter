f44b0458bb37af650a3a0d04cac30e89f18be46b
Chris Price <christopher.allen.price@gmail.com>
2017-03-22 00:24:42 -0400

    Add unit test for non-article links added to page

--- test/client/content.test.js
+++ test/client/content.test.js
@@ -33,4 +33,25 @@ describe("main content script", () =>{
 
     expect(BiasIndicatorHelper.addIndicatorToArticle).toBeCalled();
   });
+
+  test("does not add indicator to non-articles as they are added to the DOM", () => {
+    require("../../src/js/content");
+
+    jest.mock("../../src/js/client/bias-indicator-helper");
+    const BiasIndicatorHelper = require("../../src/js/client/bias-indicator-helper").default;
+    BiasIndicatorHelper.addIndicatorToArticle = jest.fn();
+
+    let eventTarget = document.createElement("a");
+    eventTarget.className = "NOT-AN-ARTICLE";
+    let onDomNodeInsertedEvent = new Event("DOMNodeInserted", {
+      "bubbles": false,
+      "target": eventTarget
+    });
+    // TODO better way of mocking DOMNodeInserted?
+    Object.defineProperty(onDomNodeInsertedEvent, 'target', {value: eventTarget, enumerable: true});
+
+    document.dispatchEvent(onDomNodeInsertedEvent);
+
+    expect(BiasIndicatorHelper.addIndicatorToArticle).not.toBeCalled();
+  });
 });
